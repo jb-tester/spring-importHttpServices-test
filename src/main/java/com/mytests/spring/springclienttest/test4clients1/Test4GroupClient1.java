988f34169ed9b4ad367eee65445e118b471c6200
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2025-09-23 23:48:26 +0200

    minor

--- src/main/java/com/mytests/spring/springclienttest/test4clients1/Test4GroupClient1.java
+++ src/main/java/com/mytests/spring/springclienttest/test4clients1/Test4GroupClient1.java
@@ -2,7 +2,9 @@ package com.mytests.spring.springclienttest.test4clients1;
 
 
 import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
 
+@HttpExchange
 public interface Test4GroupClient1 {
     @GetExchange(url = "/test41")
     String test41();
